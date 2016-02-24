c7d6f6689fed505679e8c9fc22a172fef65c189a
Scott Preston <me@scottpreston.org>
2016-02-24 12:08:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// the react component, which subscribtes to the store and updates it's state via listen
+ReactDOM.render(
+  React.createElement('span', null, 'hello world'),
+  document.getElementById('helloDiv')
+);
